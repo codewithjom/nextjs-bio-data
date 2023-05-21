df92b46d61be954cefa0513f4270d6dd3e5fb6e2
Jom Dollesin <codewithjom@gmail.com>
2023-05-21 18:39:47 +0800

    feat: added react-hot-toast.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,12 @@
 
 import { useMultistepForm } from '../components/useMultistepForm'
 import { FormEvent, useState } from 'react'
-import Link from 'next/link'
+import { useRouter } from 'next/router'
+import toast, { Toaster } from 'react-hot-toast'
 
 import { PersonalData } from '../components/PersonalData'
 import { EducationalBackground } from '../components/EducationalBackground'
 import { EmploymentRecord } from '../components/EmploymentRecord'
-import { useRouter } from 'next/router'
 
 type FormData = {
   positionDesired: string
@@ -127,7 +127,7 @@ export default function Home() {
   const router = useRouter()
 
   const refreshData = () => {
-    router.replace(router.asPath)
+    router.push('/')
   }
 
   async function onSubmit(e: FormEvent) {
@@ -191,59 +191,13 @@ export default function Home() {
         positionTo2: ''
       })
       refreshData()
+      toast.success('Account has been created')
     })
   }
 
   return (
     <main>
-      <nav className='bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600'>
-        <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-          <div className='flex md:order-2'>
-            <button
-              type='button'
-              className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-            >
-              Download
-            </button>
-          </div>
-          <div className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1' id='navbar-sticky'>
-            <ul className='flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
-              <li>
-                <Link
-                  href='#'
-                  className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500'
-                >
-                  Dashboard
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href='#'
-                  className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-                >
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href='#'
-                  className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-                >
-                  Services
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href='#'
-                  className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-                >
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Toaster />
       <div className='relative p-8 m-8 mt-28 bg-white border border-black rounded-lg lg:mx-52'>
         <form onSubmit={onSubmit}>
           <div className='absolute top-2 right-4'>
